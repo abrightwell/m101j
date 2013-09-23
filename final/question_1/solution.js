b5380c82a02ec49859f2cbd2eb6ba44ef798d68a
Adam Brightwell <abrightwell@lce.com>
2013-09-23 07:46:14 -0400

    added comments to question 1 solution.

--- final/question_1/solution.js
+++ final/question_1/solution.js
@@ -1,3 +1,15 @@
+/*
+ * Solution to Question #1 of M101J Final Exam.
+ *
+ * Author: Adam Brightwell
+ * Date:   September 10, 2013
+ *
+ * Construct a query to calculate the number of messages sent by 
+ * Andrew Fastow, CFO, to Jeff Skilling, the president. Andrew Fastow's 
+ * email addess was andrew.fastow@enron.com. Jeff Skilling's email was 
+ * jeff.skilling@enron.com. 
+ */
+
 use enron
 
 db.messages.aggregate([
